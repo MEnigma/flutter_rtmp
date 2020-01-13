7060b9108f73f2e129dc92f0c9726e722c9fcec0
MEnigma <mkw666311@163.com>
2020-01-13 18:27:53 +0800

    [Mark]修改工厂manager类型

--- android/src/main/kotlin/holo/mark/flutter_rtmp/manager.kt
+++ android/src/main/kotlin/holo/mark/flutter_rtmp/manager.kt
@@ -20,13 +20,13 @@ import kotlin.math.max
 import kotlin.math.min
 
 class RtmpFactory : PlatformViewFactory(StandardMessageCodec()) {
-    lateinit var manager: RtmpManager? = null
+    lateinit var manager: RtmpManager = null
     override fun create(context: Context?, viewId: Int, args: Any?): PlatformView {
         println("[ RTMP ] enter factory $context, viewid : $viewId")
         if( manager == null){
             manager = RtmpManager(context)
         }
-        return manager ?: RtmpManager(context)
+        return manager
     }
 
 }
