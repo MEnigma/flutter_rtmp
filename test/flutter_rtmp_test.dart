1ff60435d2e9138d62e9c0589234597a76a5a4cf
djuro <lecicdjuro@gmail.com>
2020-02-18 10:31:39 +0100

    permission version bump

--- test/flutter_rtmp_test.dart
+++ test/flutter_rtmp_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_rtmp/flutter_rtmp.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('flutter_rtmp');
@@ -16,6 +15,4 @@ void main() {
   tearDown(() {
     channel.setMockMethodCallHandler(null);
   });
-
-
 }
